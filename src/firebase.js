6d8f3aed8da26f3ecb562a6b07886f51a379f5c2
Shivam <shivamk08001@gmail.com>
2024-06-24 13:11:42 +0530

    Initial Commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCi6lt4qsmjNRZogxb3Xs10h7jlkAZ3yg',
+  apiKey:process.env.API_KEY,
   authDomain: "assisthub-667b1.firebaseapp.com",
   projectId: "assisthub-667b1",
   storageBucket: "assisthub-667b1.appspot.com",
